5ddddc4a80a54033d9ad222821c30301615210c3
cipdv <cip.devries@gmail.com>
2020-02-28 12:15:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var gradient = document.getElementById("gradient");
+
+function setGradient () {
+  gradient.style.background="linear-gradient(to right, " + color1.value + ", " + color2.value + ")";  
+}
+
+color1.addEventListener("input", setGradient)
+
+color2.addEventListener("input", setGradient)
